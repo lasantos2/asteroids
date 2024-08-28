ca00296f897319e0ca571d4739506ee3b1b370da
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2024-08-28 01:38:31 -0500

    Ch2: Modules

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import constants
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width: " + str(constants.SCREEN_WIDTH))
+    print("Screen height: " + str(constants.SCREEN_HEIGHT))
+
+
+if __name__ == "__main__":
+    main()
+
+
