9f86361ecb45d3d7639d357f71453249a64d1889
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2024-08-28 23:33:07 -0500

    Movement and using groups

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    updatable_group.add(player)
+    drawable_group.add(player)
+
     while(True):
 
         # Check for player inputs
@@ -22,6 +27,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for updatable in updatable_group:
+            updatable.update(dt)
 
         #game loop
 
@@ -30,8 +38,10 @@ def main():
         
         screen.fill(pygame.Color(0,0,0))
         
-        player.draw(screen)
-        
+        for drawable in drawable_group:
+            drawable.draw(screen)
+
+
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
